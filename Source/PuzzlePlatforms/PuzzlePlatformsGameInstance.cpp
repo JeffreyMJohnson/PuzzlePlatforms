f1188b254991d2438c45a2e601d4d1eca8e6b15c
Jeffrey Johnson <jefjohms@gmail.com>
2018-01-11 09:30:37 -0800

    Implemented game instance with some console functions.

--- Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -2,6 +2,36 @@
 
 #include "PuzzlePlatformsGameInstance.h"
 
+#include "Engine/Engine.h"
 
 
 
+
+UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Game instance constructor fired."));
+
+}
+
+void UPuzzlePlatformsGameInstance::Init()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Game instance Init() fired."));
+}
+
+void UPuzzlePlatformsGameInstance::Host()
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("Hosting..."));
+	}
+		
+}
+
+void UPuzzlePlatformsGameInstance::Connect(FString Address)
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, FString::Printf(TEXT("Connecting to %s"), *Address));
+	}
+}
